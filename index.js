c3d16ea65d3fe5264f1dd44b920ae662760ccfdb
Nathaniel330 <nathaniel.valdez.soriano@gmail.com>
2023-07-17 03:10:53 +0800

    repo for backend tests - node

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+    console.log(note)
+    response.json(note)
+})
+
+app.put('/api/notes:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(req.body)
+})
+
+
+
+app.listen(process.env.PORT || 3001, () => `running at port 3001`)
